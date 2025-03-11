ee8dadd204d33a6bdf5b831c186af640177973d9
Walter Newell <goodgands123@gmail.com>
2025-03-11 03:46:32 -0500

    blank commit

---
+++ Compnents/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='text-myPurple'>
+        Hello World
+        </nav>
+    
+      
+    </div>
+  )
+}
+
+export default Navbar 
+
